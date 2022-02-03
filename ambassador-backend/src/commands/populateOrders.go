7ccab7fe42ff1423757ad1d63d12a7531ea640a1
AEMLoviji <aemloviji@gmail.com>
2022-02-03 17:52:55 +0300

    feat: add orders and links endpoint

---
+++ ambassador-backend/src/commands/populateOrders.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"ambassador/src/database"
+	"ambassador/src/models"
+	"math/rand"
+
+	"github.com/bxcodec/faker/v3"
+)
+
+func main() {
+	// Call it inside application container to successfully establish a connection with DB
+	// Example: docker exec -it <name_of_container> sh
+	// and then call `go run src/commans/populateUsers.go`
+	database.Connect()
+
+	for i := 0; i < 30; i++ {
+		var orderItems []models.OrderItem
+
+		for j := 0; j < rand.Intn(5); j++ {
+			price := float64(rand.Intn(90) + 10)
+			qty := uint(rand.Intn(5))
+
+			orderItems = append(orderItems, models.OrderItem{
+				ProductTitle:      faker.Word(),
+				Price:             price,
+				Quantity:          qty,
+				AdminRevenue:      0.9 * price * float64(qty),
+				AmbassadorRevenue: 0.1 * price * float64(qty),
+			})
+		}
+
+		database.DB.Create(&models.Order{
+			UserId:          uint(rand.Intn(30) + 1),
+			Code:            faker.Username(),
+			AmbassadorEmail: faker.Email(),
+			FirstName:       faker.FirstName(),
+			LastName:        faker.LastName(),
+			Email:           faker.Email(),
+			Complete:        true,
+			OrderItems:      orderItems,
+		})
+	}
+}

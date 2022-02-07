3df27b8431f6cda8953942aa51f8105055542357
AEMLoviji <aemloviji@gmail.com>
2022-02-07 14:29:06 +0300

    feat: register and login for ambassadors also

--- ambassador-backend/src/controllers/authController.go
+++ ambassador-backend/src/controllers/authController.go
@@ -4,10 +4,9 @@ import (
 	"ambassador/src/database"
 	"ambassador/src/middlewares"
 	"ambassador/src/models"
-	"strconv"
+	"strings"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -29,7 +28,7 @@ func Register(c *fiber.Ctx) error {
 		FirstName:    data["first_name"],
 		LastName:     data["last_name"],
 		Email:        data["email"],
-		IsAmbassador: false,
+		IsAmbassador: strings.Contains(c.Path(), "/api/ambassador"),
 	}
 	user.SetPassword(data["password"])
 
@@ -63,12 +62,25 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	payload := jwt.StandardClaims{
-		Subject:   strconv.Itoa(int(user.Id)),
-		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	isAmbassdor := strings.Contains(c.Path(), "/api/ambassador")
+
+	var scope string
+
+	if isAmbassdor {
+		scope = "ambassador"
+	} else {
+		scope = "admin"
 	}
 
-	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, payload).SignedString([]byte("secret"))
+	// do not allow ambassador user to login as admin
+	if !isAmbassdor && user.IsAmbassador {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthorized",
+		})
+	}
+
+	token, err := middlewares.GenerateJWT(user.Id, scope)
 
 	if err != nil {
 		c.Status(fiber.StatusBadRequest)
--- ambassador-backend/src/middlewares/auth.go
+++ ambassador-backend/src/middlewares/auth.go
@@ -2,19 +2,38 @@ package middlewares
 
 import (
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 )
 
+const SecretKey = "secret"
+
+type ClaimsWithScope struct {
+	jwt.StandardClaims
+	Scope string
+}
+
 func IsAuthenticated(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
 
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte("secret"), nil
+	token, err := jwt.ParseWithClaims(cookie, &ClaimsWithScope{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
 	})
 
 	if err != nil || !token.Valid {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+	payload := token.Claims.(*ClaimsWithScope)
+	isAmbassador := strings.Contains(c.Path(), "/api/ambassador")
+
+	if (payload.Scope == "admin" && isAmbassador) || (payload.Scope == "ambassador" && !isAmbassador) {
 		c.Status(fiber.StatusUnauthorized)
 		return c.JSON(fiber.Map{
 			"message": "unauthorized",
@@ -24,18 +43,30 @@ func IsAuthenticated(c *fiber.Ctx) error {
 	return c.Next()
 }
 
+func GenerateJWT(id uint, scope string) (string, error) {
+	payload := ClaimsWithScope{
+		StandardClaims: jwt.StandardClaims{
+			Subject:   strconv.Itoa(int(id)),
+			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+		},
+		Scope: scope,
+	}
+
+	return jwt.NewWithClaims(jwt.SigningMethodHS256, payload).SignedString([]byte(SecretKey))
+}
+
 func GetUserId(c *fiber.Ctx) (uint, error) {
 	cookie := c.Cookies("jwt")
 
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte("secret"), nil
+	token, err := jwt.ParseWithClaims(cookie, &ClaimsWithScope{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
 	})
 
 	if err != nil {
 		return 0, err
 	}
 
-	payload := token.Claims.(*jwt.StandardClaims)
+	payload := token.Claims.(*ClaimsWithScope)
 
 	id, _ := strconv.Atoi(payload.Subject)
 
